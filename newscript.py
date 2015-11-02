f4504dc721f7ef17dd8d739c849916db6e19433a
Kim Hermans <kimhermans@iusd.org>
2015-11-02 15:31:26 -0800

    "Committed"

---
+++ newscript.py
@@ -0,0 +1,19 @@
+
+
+from datetime import datetime
+max =max( 3, 6, 1, 2 )
+print max
+now = datetime.now()
+print now.year
+print now.month
+print now.day
+
+x = raw_input( "enter a number:")
+print str( x )
+my_int = 8
+my_bool = True
+my_word = "Eh!"
+print "hello there"
+print my_int, my_word
+print my_bool
+print my_word
